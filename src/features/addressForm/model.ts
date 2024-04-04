dfa0562a0f0d4ecb0fdbe707a5ccff0f2189032f
YanLobat <yanlavryushev@gmail.com>
2024-04-04 03:18:42 +0100

    update address input in store

--- src/features/addressForm/model.ts
+++ src/features/addressForm/model.ts
@@ -1,7 +1,11 @@
-import { createStore, createEvent } from 'effector'
+import { createStore, createEvent, sample } from 'effector';
 
 export const $addressFormError = createStore<string>('')
 export const $address = createStore<string>('')
 export const changeAddress = createEvent<string>()
 
 $addressFormError.reset(changeAddress)
+sample({
+    clock: changeAddress,
+    target: $address
+})
